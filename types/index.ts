3500b5b7cc103fd5fb893f053790fe7fd0e4421e
kristinnornk <58362287+kristinnornk@users.noreply.github.com>
2023-08-31 11:37:43 +0000

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,16 @@
+/**
+ * @description Person type
+ */
 export type Person = {
   name: string;
   age: number;
   address: Address;
 };
 
+/**
+ * @description Address type
+ * @memberof Person
+ */
 export type Address = {
   street: string;
   city: string;
